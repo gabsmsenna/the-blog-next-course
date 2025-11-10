5840b8fe8c2c79039783c422b9319f424bbc55e5
Gabriel Senna <gabrielmagalhaes3344@gmail.com>
2025-11-10 14:09:35 -0300

    feat: PostHeading component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { Container } from "@/components/Container";
 import { Header } from "@/components/Header";
 import Link from "next/link";
 import Image from "next/image";
+import { PostHeading } from "@/components/PostHeading";
 
 export default function Home() {
   return (
@@ -16,13 +17,20 @@ export default function Home() {
         className="w-full h-full overflow-hidden rounded-2xl" 
         href="#">
           <Image 
-          className="group-hover:scale-105 transition duration-300 "
+          className="w-full h-full object-cover object-center group-hover:scale-105 transition duration-300 "
           src='/images/bryen_0.png' 
           width={1200}
           height={720}
-          alt="Título post"/>
+          alt="Título post"
+          priority={true}
+          />
         </Link>
-        <div>
+        
+        <div className="flex flex-col gap-4 sm:justify-center">
+          <time className="text-slate-600 text-sm/tight" dateTime="2025-04-20">20/04/2025 10:00</time>
+
+          <PostHeading as="h1" url="#">Título do post</PostHeading>
+          
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, libero suscipit,
           odio, aliquid laudantium incidunt beatae nulla nemo sunt tempora aliquam 
           fugiat perspiciatis quasi quaerat voluptate animi. Incidunt, autem temporibus!
