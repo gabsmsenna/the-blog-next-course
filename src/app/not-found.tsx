8b6b448079cbfac9cf1f447abeb3a6637b1042ce
Gabriel Senna <gabrielmagalhaes3344@gmail.com>
2025-11-14 09:47:32 -0300

    feat: not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFoundPage() {
+  return (
+    <div className="min-h-80 text-slate-900 mb-16 p-8 rounded-xl flex items-center justify-center text-center">
+      <div>
+        <h1 className="text-7xl/normal mb-4 font-extrabold">404</h1>
+        <p>Erro 404 - Esta página não pode ser encontra</p>
+      </div>
+    </div>
+  );
+}
