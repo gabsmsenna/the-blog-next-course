4ead74eb618005392736deb120e72ecf12d7d2d8
Gabriel Senna <gabrielmagalhaes3344@gmail.com>
2025-11-17 19:30:22 -0300

    feat: error message component

---
+++ src/components/ErrorMessage/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+type ErrorMessageProps = {
+    pageTitle: string
+    contentTitle: string
+    content: React.ReactNode
+}
+
+export default function ErrorMessage({pageTitle, contentTitle, content}: ErrorMessageProps) {
+  return (
+    <>
+    <title>{pageTitle}</title>
+    <div className="min-h-80 bg-slate-900 text-slate-100 mb-16 p-8 rounded-xl flex items-center justify-center text-center">
+      <div>
+        <h1 className="text-7xl/normal mb-4 font-extrabold">{contentTitle}</h1>
+        <div>{content}</div>
+      </div>
+    </div>
+    </>
+  );
+}
