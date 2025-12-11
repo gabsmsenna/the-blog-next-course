80b39417ef00f17b85ea2bc619517349a9c0aa2f
Gabriel Senna <gabrielmagalhaes3344@gmail.com>
2025-12-11 16:22:21 -0300

    feat: Displays a preview of the URL and image.

--- src/components/admin/ImageUploader/index.tsx
+++ src/components/admin/ImageUploader/index.tsx
@@ -3,12 +3,15 @@
 import { uploadImage } from "@/actions/upload/upload-image-action";
 import { Button } from "@/components/Button";
 import { IMAGE_UPLOAD_MAX_FILE_SIZE } from "@/lib/post/constants";
+import { set } from "date-fns";
 import { ImageUpIcon } from "lucide-react";
-import { startTransition, useRef } from "react";
+import { startTransition, useRef, useState, useTransition } from "react";
 import { toast } from "react-toastify";
 
 export function ImageUploader() {
   const fileInputRef = useRef<HTMLInputElement>(null);
+  const [isUploading, startTransition] = useTransition();
+  const [imgUrl, setImgUrl] = useState("");
 
   function handleChooseFile() {
     if (!fileInputRef.current) return;
@@ -17,20 +20,29 @@ export function ImageUploader() {
   }
 
   function handleChange() {
-    if (!fileInputRef.current) return;
+    if (!fileInputRef.current) {
+      setImgUrl("");
+      return;
+    }
 
     const fileInput = fileInputRef.current;
     const file = fileInput?.files?.[0];
 
-    if (!file) return;
+    if (!file) {
+      setImgUrl("");
+      return;
+    }
 
     if (file.size > IMAGE_UPLOAD_MAX_FILE_SIZE) {
-      toast.error(`O tamanho máximo permitido é de ${
-        IMAGE_UPLOAD_MAX_FILE_SIZE / 1024
-      } KB.`);
+      toast.error(
+        `O tamanho máximo permitido é de ${
+          IMAGE_UPLOAD_MAX_FILE_SIZE / 1024
+        } KB.`
+      );
       fileInput.value = "";
+      setImgUrl("");
       return;
-    };
+    }
 
     const formData = new FormData();
     formData.append("file", file);
@@ -40,21 +52,40 @@ export function ImageUploader() {
 
       if (result.error) {
         toast.error(`Erro ao enviar a imagem: ${result.error}`);
+        fileInput.value = "";
+        setImgUrl("");
         return;
       }
 
+      setImgUrl(result.url);
       toast.success("Imagem enviada com sucesso!");
-    })
+    });
 
     fileInput.value = "";
   }
 
   return (
-    <div className="flex flex-col gap-2 py-4">
-      <Button onClick={handleChooseFile} type="button" className="self-start">
+    <div className="flex flex-col gap-4 py-4">
+      <Button
+        onClick={handleChooseFile}
+        type="button"
+        className="self-start"
+        disabled={isUploading}
+      >
         <ImageUpIcon />
         Enviar uma imagem
       </Button>
+
+      {!!imgUrl && (
+        <div className="flex flex-col gap-4">
+          <p>
+            <b>URL:</b> {imgUrl}
+          </p>
+
+          <img className="rounded-lg" src={imgUrl} alt="Imagem enviada"/>
+        </div>
+      )}
+
       <input
         onChange={handleChange}
         ref={fileInputRef}
